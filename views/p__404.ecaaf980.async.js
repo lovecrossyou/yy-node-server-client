f1693fa51bfa76b274543a9d1673d5f8270815a6
lovecross <lovecrossyoutt@hotmail.com>
2019-01-06 17:12:33 +0800

    xx

---
+++ views/p__404.ecaaf980.async.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[3],{w2l6:function(e,n,t){"use strict";var u=t("FfR7");Object.defineProperty(n,"__esModule",{value:!0}),n.default=void 0;var d=u(t("rdAL")),a=function(){return d.default.createElement("div",null,"404\u9875\u9762")};n.default=a}}]);
--- yy_app.js
+++ yy_app.js
@@ -12,10 +12,10 @@ yy_app.set('views', './views'); // 指定视图所在的位置
 //
 yy_app.engine('html', ejs.__express);
 yy_app.set('view engine', 'html');
-yy_app.use('/yy', express.static(__dirname + "/views/"));
-yy_app.use('/h5', proxys);
+yy_app.use('/', express.static(__dirname + "/views/"));
+yy_app.use('/api', proxys);
 
-var server = http.createServer(yy_app).listen(7003, function () {
+var server = http.createServer(yy_app).listen(7004, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('ranning at host', host);
